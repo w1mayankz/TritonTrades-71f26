91d7d847434e6c16daba21590f9c1526e54d2f81
Mayank <w1mayankz@gmail.com>
2026-05-09 19:17:37 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import StartSection from './components/StartSection'
+import FeaturesChess from './components/FeaturesChess'
+import FeaturesGrid from './components/FeaturesGrid'
+import Stats from './components/Stats'
+import Testimonials from './components/Testimonials'
+import CtaFooter from './components/CtaFooter'
+
+export default function App() {
+  return (
+    <div className="bg-black min-h-screen w-full overflow-x-hidden">
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+        <div className="bg-black">
+          <StartSection />
+          <FeaturesChess />
+          <FeaturesGrid />
+          <Stats />
+          <Testimonials />
+          <CtaFooter />
+        </div>
+      </div>
+    </div>
+  )
+}
