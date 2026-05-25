b0b2159a6ab74c9867b381002e41c5148e900ce3
Mayank <w1mayankz@gmail.com>
2026-05-25 19:14:11 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section
       id="home"
       className="relative overflow-visible"
-      style={{ height: 700 }}
+      style={{ height: 550 }}
     >
       <div className="absolute inset-0 bg-black/5 z-0" />
 
