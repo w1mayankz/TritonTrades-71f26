f3cd6e72725efe960743632e1f15871a0ea5b71c
Mayank <w1mayankz@gmail.com>
2026-07-18 09:20:59 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ export default function Hero() {
             delay={100}
             animateBy="words"
             direction="bottom"
-            className="text-4xl md:text-7xl lg:text-[5.5rem] font-['Inter_Tight'] font-bold not-italic text-white leading-[0.8] tracking-[-4px] justify-center"
+            className="text-5xl md:text-7xl lg:text-[5.5rem] font-['Inter_Tight'] font-bold not-italic text-white leading-[0.8] tracking-[-4px] justify-center"
           />
           
           {/* Bottom Line (Mixed Fonts) */}
@@ -55,14 +55,14 @@ export default function Hero() {
               delay={200}
               animateBy="words"
               direction="bottom"
-              className="text-4xl md:text-7xl lg:text-[5.5rem] font-['Inter_Tight'] font-bold not-italic text-white leading-[0.8] tracking-[-4px] justify-center"
+              className="text-5xl md:text-7xl lg:text-[5.5rem] font-['Inter_Tight'] font-bold not-italic text-white leading-[0.8] tracking-[-4px] justify-center"
             />
             <BlurText
               text="random."
               delay={300}
               animateBy="words"
               direction="bottom"
-              className="text-4xl md:text-7xl lg:text-[5.5rem] font-heading italic text-white leading-[0.8] tracking-[-4px] justify-center"
+              className="text-5xl md:text-7xl lg:text-[5.5rem] font-heading italic text-white leading-[0.8] tracking-[-4px] justify-center"
             />
           </div>
         </div>
